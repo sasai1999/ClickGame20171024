7198937bfe6857188abe80dcbc454849b7e5bdc6
Masanobu Sasai <nkc.masanobu.sasai@gmail.com>
2017-10-26 10:17:56 +0900

    GameGenerator作成

---
+++ Assets/SquareController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareController : MonoBehaviour {
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Shoot(new Vector3(0, 0,0));
+    }
+
+    void OnCllisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
